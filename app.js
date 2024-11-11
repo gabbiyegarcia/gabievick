7d4501f5a74866c7c7f33aaceb448202649dbcc6
gabbiyegarcia <162997167+gabbiyegarcia@users.noreply.github.com>
2024-11-11 10:54:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-alert( "BOAS-VINDAS AO JOGO DO NÚMERO SECRETO")
-let numerosecretom =28;
-prompt( "ESCOLHA UM NÚERO DE 1 A 100")
-let chute;
+alert( "BOAS-VINDAS AO JOGO DO NÚMERO SECRETO");
+let numeroSecreto =28;
+let chute = prompt( "ESCOLHA UM NÚERO DE 1 A 100");
+
+if (chute == numeroSecreto) {
+    alert ("PARABÉNS, Você descobriu o número secreto")
+} else{
+    alert(" Você errou! " )
+}
+if (chute > numeroSecreto) {
+    alert ("Você está quase lá, tente um valor menor")
+}
+if (chute < numeroSecreto) {
+    alert ("você está quase lá, tente um valor maior ")
+}
